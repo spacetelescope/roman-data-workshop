46c0c6f817a24f1138677d85a14716fb6c61ddda
zacharyburnett <zburnett@stsci.edu>
2022-06-24 12:11:38 -0400

    add new files from Box

--- data/download.py
+++ data/download.py
@@ -5,6 +5,15 @@ from astropy.utils.data import download_file
 
 REMOTE_URL = 'https://data.science.stsci.edu/redirect/Roman/Roman_Data_Workshop/'
 REMOTE_PATHS = [
+    'ExampleData/roman_wfi_dark_0227.asdf',
+    'ExampleData/roman_wfi_linearity_0055.asdf',
+    'ExampleData/roman_wfi_saturation_0078.asdf',
+    'ExampleData/roman_wfi_flat_0227.asdf',
+    'ExampleData/roman_wfi_readnoise_0189.asdf',
+    'ExampleData/roman_wfi_gain_0056.asdf',
+    'ExampleData/roman_wfi_mask_0074.asdf',
+    'ExampleData/roman_wfi_photom_0034.asdf',
+    'ExampleData/roman_wfi_distortion_0008.asdf',
     'ExampleData/r0000101001001001001_01101_0001_WFI01_uncal.asdf',
     'ExampleData/r0000101001001001001_01101_0001_WFI01_cal.asdf',
 ]
