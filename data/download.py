585afd439429b6079c35d3457d1447c4b922963b
Nadia Dencheva <nadia.dencheva@gmail.com>
2022-09-06 14:37:17 -0400

    add notebook example of JWST NIRISS grism processing

--- data/download.py
+++ data/download.py
@@ -12,10 +12,17 @@ REMOTE_PATHS = [
     "ExampleData/roman_wfi_readnoise_0189.asdf",
     "ExampleData/roman_wfi_gain_0056.asdf",
     "ExampleData/roman_wfi_mask_0074.asdf",
-    "ExampleData/roman_wfi_photom_0056.asdf",
+    "ExampleData/roman_wfi_photom_0034.asdf",
     "ExampleData/roman_wfi_distortion_0008.asdf",
     "ExampleData/r0000101001001001001_01101_0001_WFI01_uncal.asdf",
     "ExampleData/r0000101001001001001_01101_0001_WFI01_cal.asdf",
+    "ExampleData/jw01448013001_02105_00001_nis_rate.fits",
+    "ExampleData/jw01448013001_02106_00001_nis_rate.fits",
+    "ExampleData/jw01448013001_02106_00001_nis_cat.ecsv",
+    "ExampleData/jwst_niriss_distortion_0013.asdf",
+    "ExampleData/jwst_niriss_filteroffset_0002.asdf",
+    "ExampleData/jwst_niriss_specwcs_0018.asdf",
+    "ExampleData/jwst_niriss_wavelengthrange_0002.asdf",    
 ]
 LOCAL_DIRECTORY = Path(__file__).parent
 
