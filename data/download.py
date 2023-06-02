8363d35261a0b181a4bad9e59609b3b964ee89b3
Nadia Dencheva <nadia.dencheva@gmail.com>
2023-06-02 10:55:16 -0400

    Update notebooks with new data for Build 10 (#22)
    * Update notebooks with new data for Build 10

--- data/download.py
+++ data/download.py
@@ -5,15 +5,15 @@ from astropy.utils.data import download_file
 
 REMOTE_URL = "https://data.science.stsci.edu/redirect/Roman/Roman_Data_Workshop/"
 REMOTE_PATHS = [
-    "ExampleData/roman_wfi_dark_0427.asdf",
-    "ExampleData/roman_wfi_linearity_0179.asdf",
-    "ExampleData/roman_wfi_saturation_0193.asdf",
+    "ExampleData/roman_wfi_dark_0549.asdf",
+    "ExampleData/roman_wfi_linearity_0215.asdf",
+    "ExampleData/roman_wfi_saturation_0229.asdf",
     "ExampleData/roman_wfi_flat_0227.asdf",
-    "ExampleData/roman_wfi_readnoise_0368.asdf",
-    "ExampleData/roman_wfi_gain_0169.asdf",
+    "ExampleData/roman_wfi_readnoise_0440.asdf",
+    "ExampleData/roman_wfi_gain_0205.asdf",
     "ExampleData/roman_wfi_mask_0074.asdf",
     "ExampleData/roman_wfi_photom_0056.asdf",
-    "ExampleData/roman_wfi_distortion_0008.asdf",
+    "ExampleData/roman_wfi_distortion_0054.asdf",
     "ExampleData/r0000101001001001001_01101_0001_WFI01_uncal.asdf",
     "ExampleData/r0000101001001001001_01101_0001_WFI01_cal.asdf",
     "ExampleData/jw01448013001_02105_00001_nis_rate.fits",
@@ -22,7 +22,7 @@ REMOTE_PATHS = [
     "ExampleData/jwst_niriss_distortion_0013.asdf",
     "ExampleData/jwst_niriss_filteroffset_0002.asdf",
     "ExampleData/jwst_niriss_specwcs_0018.asdf",
-    "ExampleData/jwst_niriss_wavelengthrange_0002.asdf",    
+    "ExampleData/jwst_niriss_wavelengthrange_0002.asdf",
 ]
 LOCAL_DIRECTORY = Path(__file__).parent
 
