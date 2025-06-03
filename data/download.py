323e3d465be13e34fe81d655c11734e8310bb364
Paul Huwe <42071634+PaulHuwe@users.noreply.github.com>
2025-06-03 12:33:40 -0400

    Build 18 (#38)
    * Initial commit
    
    * Corrected STPSF location
    
    * Adjusted CRDS pointer.

--- data/download.py
+++ data/download.py
@@ -8,34 +8,49 @@ from astropy.utils.data import download_file
 
 REMOTE_URL = "https://data.science.stsci.edu/redirect/Roman/Roman_Data_Workshop/"
 REMOTE_PATHS = [
-    "ExampleData/Build17/r0000101001001001001_0001_wfi01_uncal.asdf",
-    "ExampleData/Build17/r0000101001001001001_0001_wfi01_cal.asdf",
-    "ExampleData/Build17/r0000101001001001001_0001_wfi01_cat.asdf",
-    "ExampleData/Build17/r0000101001001001001_0001_wfi01_segm.asdf",
-    "ExampleData/Build17/r0000101001001001001_p_v01001001001_r274dp63x31y80_f158_asn.json",
-    "ExampleData/Build17/r0000101001001001001_p_v01001001001_r274dp63x31y81_f158_asn.json",
-    "ExampleData/Build17/r0000101001001001001_p_v01001001001_r274dp63x31y82_f158_asn.json",
-    "ExampleData/Build17/r0000101001001001001_p_v01001001001_r274dp63x32y80_f158_asn.json",
-    "ExampleData/Build17/r0000101001001001001_p_v01001001001_r274dp63x32y81_f158_asn.json",
-    "ExampleData/Build17/r0000101001001001001_p_v01001001001_r274dp63x31y80_f158_coadd.asdf",
-    "ExampleData/Build17/r0000101001001001001_p_v01001001001_r274dp63x31y81_f158_coadd.asdf",
-    "ExampleData/Build17/r0000101001001001001_p_v01001001001_r274dp63x31y82_f158_coadd.asdf",
-    "ExampleData/Build17/r0000101001001001001_p_v01001001001_r274dp63x32y80_f158_coadd.asdf",
-    "ExampleData/Build17/r0000101001001001001_p_v01001001001_r274dp63x32y81_f158_coadd.asdf",
+    "ExampleData/Build18/r0000101001001001001_0001_wfi01_f158_uncal.asdf",
+    "ExampleData/Build18/r0000101001001001001_0001_wfi01_f158_cal.asdf",
+    "ExampleData/Build18/r0000101001001001001_0001_wfi01_f158_cat.parquet",
+    "ExampleData/Build18/r0000101001001001001_0001_wfi01_f158_wcs.asdf",
+    "ExampleData/Build18/r0000101001001001001_0001_wfi01_f158_segm.asdf",
+    
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x48y69_f158_asn.json",
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x48y70_f158_asn.json",
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x49y69_f158_asn.json",
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x49y70_f158_asn.json",
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x50y69_f158_asn.json",
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x50y70_f158_asn.json",
+    
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x48y69_f158_coadd.asdf",
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x48y70_f158_coadd.asdf",
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x49y69_f158_coadd.asdf",
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x49y70_f158_coadd.asdf",
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x50y69_f158_coadd.asdf",
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x50y70_f158_coadd.asdf",
+    
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x48y69_f158_cat.parquet",
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x48y70_f158_cat.parquet",
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x49y69_f158_cat.parquet",
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x49y70_f158_cat.parquet",
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x50y69_f158_cat.parquet",
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x50y70_f158_cat.parquet",
+    
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x48y69_f158_segm.asdf",
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x48y70_f158_segm.asdf",
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x49y69_f158_segm.asdf",
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x49y70_f158_segm.asdf",
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x50y69_f158_segm.asdf",
+    "ExampleData/Build18/r00001_p_v01001001001001_270p65x50y70_f158_segm.asdf",
+    
+    "ExampleData/Build18/r0000201001001001001_0001_wfi01_grism_uncal.asdf",
+    "ExampleData/Build18/r0000201001001001001_0001_wfi01_grism_cal.asdf",
+    "ExampleData/Build18/r0000201001001001001_0001_wfi01_grism_wcs.asdf",
+    
+    "ExampleData/Build18/r0000301001001001001_0001_wfi01_prism_uncal.asdf",
+    "ExampleData/Build18/r0000301001001001001_0001_wfi01_prism_cal.asdf",
+    "ExampleData/Build18/r0000301001001001001_0001_wfi01_prism_wcs.asdf",
+    
     "ExampleData/Build16/new_distortion.asdf",
-    "ExampleData/Build17/r0000101001001001001_p_v01001001001_r274dp63x31y80_f158_cat.asdf",
-    "ExampleData/Build17/r0000101001001001001_p_v01001001001_r274dp63x31y81_f158_cat.asdf",
-    "ExampleData/Build17/r0000101001001001001_p_v01001001001_r274dp63x31y82_f158_cat.asdf",
-    "ExampleData/Build17/r0000101001001001001_p_v01001001001_r274dp63x32y80_f158_cat.asdf",
-    "ExampleData/Build17/r0000101001001001001_p_v01001001001_r274dp63x32y81_f158_cat.asdf",
-    "ExampleData/Build17/r0000101001001001001_p_v01001001001_r274dp63x31y80_f158_segm.asdf",
-    "ExampleData/Build17/r0000101001001001001_p_v01001001001_r274dp63x31y81_f158_segm.asdf",
-    "ExampleData/Build17/r0000101001001001001_p_v01001001001_r274dp63x32y80_f158_segm.asdf",
-    "ExampleData/Build17/r0000101001001001001_p_v01001001001_r274dp63x32y81_f158_segm.asdf",
-    "ExampleData/Build17/r0000201001001001001_0001_wfi01_uncal.asdf",
-    "ExampleData/Build17/r0000201001001001001_0001_wfi01_cal.asdf",
-    "ExampleData/Build17/r0000301001001001001_0001_wfi01_uncal.asdf",
-    "ExampleData/Build17/r0000301001001001001_0001_wfi01_cal.asdf",
     "ExampleData/jw01448013001_02105_00001_nis_rate.fits",
     "ExampleData/jw01448013001_02106_00001_nis_rate.fits",
     "ExampleData/jw01448013001_02106_00001_nis_cat.ecsv",
@@ -45,7 +60,7 @@ REMOTE_PATHS = [
     "ExampleData/jwst_niriss_wavelengthrange_0002.asdf",
 ]
 LOCAL_DIRECTORY = Path(__file__).parent
-WEBBPSF_DATA = Path('./data')/'Webbpsf-data-LATEST.tar.gz'
+STPSF_DATA = Path('./data')/'stpsf-data-LATEST.tar.gz'
 
 
 def download_data(overwrite: bool = False):
@@ -56,13 +71,13 @@ def download_data(overwrite: bool = False):
             filename = download_file(REMOTE_URL + remote_path)
             shutil.move(filename, local_path)
 
-    response = requests.get('https://stsci.box.com/shared/static/qxpiaxsjwo15ml6m4pkhtk36c9jgj70k.gz')
+    response = requests.get('https://stsci.box.com/shared/static/3hzmbarac5yxjt6x7gn17vz02k7c8z1d.gz')
 
-    with open(WEBBPSF_DATA, 'wb') as f:
+    with open(STPSF_DATA, 'wb') as f:
         f.write(response.content)
 
     print("Done downloading files")
 
 if __name__ == "__main__":
     download_data()
-    shutil.unpack_archive(WEBBPSF_DATA, Path('./data'))
+    shutil.unpack_archive(STPSF_DATA, Path('./data'))
