93541ef10bb17747eaa218fef97a98dd679207c5
PaulHuwe <phuwe@stsci.edu>
2026-06-09 23:21:11 -0400

    Rebase

--- data/download.py
+++ data/download.py
@@ -6,6 +6,7 @@ from astropy.utils.data import download_file
 
 REMOTE_URL = "https://data.science.stsci.edu/redirect/Roman/Roman_Data_Workshop/"
 REMOTE_PATHS = [
+<<<<<<< HEAD
     "ExampleData/Build22/r0000101001001001001_0001_wfi01_f158_uncal.asdf",
     "ExampleData/Build22/r0000101001001001001_0001_wfi01_f158_cal.asdf",
     "ExampleData/Build22/r0000101001001001001_0001_wfi01_f158_cat.parquet",
@@ -47,6 +48,49 @@ REMOTE_PATHS = [
     "ExampleData/Build22/r0000301001001001001_0001_wfi01_prism_uncal.asdf",
     "ExampleData/Build22/r0000301001001001001_0001_wfi01_prism_cal.asdf",
     "ExampleData/Build22/r0000301001001001001_0001_wfi01_prism_wcs.asdf",
+=======
+    "ExampleData/Build21/r0000101001001001001_0001_wfi01_f158_uncal.asdf",
+    "ExampleData/Build21/r0000101001001001001_0001_wfi01_f158_cal.asdf",
+    "ExampleData/Build21/r0000101001001001001_0001_wfi01_f158_cat.parquet",
+    "ExampleData/Build21/r0000101001001001001_0001_wfi01_f158_wcs.asdf",
+    "ExampleData/Build21/r0000101001001001001_0001_wfi01_f158_segm.asdf",
+    
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x69y48_f158_asn.json",
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x69y49_f158_asn.json",
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x69y50_f158_asn.json",
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x70y48_f158_asn.json",
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x70y49_f158_asn.json",
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x70y50_f158_asn.json",
+    
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x69y48_f158_coadd.asdf",
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x69y49_f158_coadd.asdf",
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x69y50_f158_coadd.asdf",
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x70y48_f158_coadd.asdf",
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x70y49_f158_coadd.asdf",
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x70y50_f158_coadd.asdf",
+    
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x69y48_f158_cat.parquet",
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x69y49_f158_cat.parquet",
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x69y50_f158_cat.parquet",
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x70y48_f158_cat.parquet",
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x70y49_f158_cat.parquet",
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x70y50_f158_cat.parquet",
+    
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x69y48_f158_segm.asdf",
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x69y49_f158_segm.asdf",
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x69y50_f158_segm.asdf",
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x70y48_f158_segm.asdf",
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x70y49_f158_segm.asdf",
+    "ExampleData/Build21/r00001_p_v01001001001001_270p65x70y50_f158_segm.asdf",
+    
+    "ExampleData/Build21/r0000201001001001001_0001_wfi01_grism_uncal.asdf",
+    "ExampleData/Build21/r0000201001001001001_0001_wfi01_grism_cal.asdf",
+    "ExampleData/Build21/r0000201001001001001_0001_wfi01_grism_wcs.asdf",
+    
+    "ExampleData/Build21/r0000301001001001001_0001_wfi01_prism_uncal.asdf",
+    "ExampleData/Build21/r0000301001001001001_0001_wfi01_prism_cal.asdf",
+    "ExampleData/Build21/r0000301001001001001_0001_wfi01_prism_wcs.asdf",
+>>>>>>> d788916 (Updates for B21.)
     
     "ExampleData/Build16/new_distortion.asdf",
 ]
